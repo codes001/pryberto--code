ced44566bf7210bf6068f7bab9e6503f49a78396
Prince Effiong <prynzwill147@gmail.com>
2024-10-18 19:51:51 +0100

    Contact

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Button } from "./ui/button";
 
 const About = () => {
   return (
-    <div id="about" className="h-screen w-full">
-      About
+    <div
+      id="about"
+      className="h-screen w-[95%] sm:w-[90%] md:leading-8 text-justify mx-auto md:w-[80%] flex flex-col items-center justify-center"
+    >
+      <div className="text-2xl text-center">
+        <h2 className="font-bold">ABOUT US</h2>
+      </div>
+
+      <div className="flex flex-col mt-4 items-center justify-center">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio autem
+          dolore unde ea neque placeat sapiente officia necessitatibus? Eveniet,
+          laudantium! Eum dolore quae odio, quod animi et obcaecati blanditiis
+          architecto, iste autem impedit id, ea beatae. Harum inventore ducimus
+          fuga veritatis illum veniam est earum. Iure eligendi voluptate
+          deleniti. Illo, dolorem inventore ratione hic amet repellendus
+          veritatis minus iusto? Veritatis adipisci minima ratione deleniti eius
+          blanditiis dolorem. Rerum aut, illum nihil at laudantium enim mollitia
+          dicta ducimus cum recusandae, dolores laboriosam ullam voluptatum qui
+          in deserunt iusto excepturi ipsum consectetur!
+        </p>
+        <Button className=" transition-all duration-100 ease-in-out bg-blue-primary !rounded-full text-white mt-4 hover:bg-transparent hover:border hover:border-blue-primary hover:text-blue-primary">
+          {" "}
+          Contact Us
+        </Button>
+      </div>
     </div>
   );
 };
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
 
 const Contact: React.FC = () => {
   return (
-    <div id="contact" className="h-screen w-full">
-      Contact
-    </div>
+    <form
+      id="contact"
+      className="h-auto w-[70%] mx-auto px-8 py-16 border flex flex-col gap-6 border-blue-primary rounded-[10px]"
+    >
+      <div>
+        <Input placeholder="Enter your name" />
+      </div>
+
+      <div>
+        <Input placeholder="Enter your email address" />
+      </div>
+
+      <div>
+        <Textarea
+          cols={12}
+          className="resize-none rounded-[5px] h-[150px]"
+          placeholder="Drop a message"
+        >
+          {" "}
+        </Textarea>
+      </div>
+
+      <Button className=" transition-all duration-100 ease-in-out bg-blue-primary !rounded-full text-white mt-4 hover:bg-transparent hover:border hover:border-blue-primary hover:text-blue-primary uppercase font-bold py-6">
+        {" "}
+        Submit
+      </Button>
+    </form>
   );
 };
 
